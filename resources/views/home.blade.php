98e2f0d1b0a1ac65f553daf53d353f560d291d0d
Nico Morero <nmorero00@gmail.com>
2020-03-04 12:53:29 -0300

    Clear Cache

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.base')
 
 @section('head')
-<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 @endsection
 
 @section('title')
