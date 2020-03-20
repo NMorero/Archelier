9fe0ed332ff7fb9debc69d0a83deca1c54d7a7ae
Tu Nombre <nmorero00@gmail.com>
2020-03-20 11:00:14 -0300

    Cambios css

--- resources/views/layouts/adminBase.blade.php
+++ resources/views/layouts/adminBase.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximun-scale=1.1" >
->>>>>>> 140b81e5be4b81d65f191f654711bef054b7bc6f
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=" {{asset('/css/master.css')}} ">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
