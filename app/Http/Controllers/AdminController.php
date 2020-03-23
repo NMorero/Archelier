74efec9cec889f4b1c96ef617667aaee21ce6255
Tu Nombre <nmorero00@gmail.com>
2020-03-23 10:39:48 -0300

    m

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -553,7 +553,7 @@ class AdminController extends Controller
 
             }
         }else if(auth()->user()->roles->rol == 'PRleader'){
-            $lead = ProjectLeaders::find($id);
+            $lead = ProjectLeaders::where('user_id', 'LIKE', $id)->get();
             $leadProjects = Projects::where('leader_id', 'LIKE', $lead[0]->id)->get();
             foreach($leadProjects as $leadProject){
                 $project = Projects::find($leadProject->id);
@@ -563,7 +563,7 @@ class AdminController extends Controller
 
             }
         }else if(auth()->user()->roles->rol == 'PRmanager'){
-            $man = ProjectManagers::find($id);
+            $man = ProjectManagers::where('user_id', 'LIKE', $id)->get();
             $manProjects = Projects::where('manager_id', 'LIKE', $man[0]->id)->get();
             foreach($manProjects as $manProject){
                 $project = Projects::find($manProject->id);
