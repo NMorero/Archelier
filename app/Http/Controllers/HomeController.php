51b17bf828e6210a8346b402a46d9f004da0ff83
Tu Nombre <nmorero00@gmail.com>
2020-04-08 11:37:08 -0300

    Cambios agenda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -511,7 +511,7 @@ class HomeController extends Controller
                 array_push($tasks[$endDate]['projects'], $taskDB);
             }
         }
-
+        
         foreach ($events as $event) {
             $endDateEvent = $event->end_date;
             if (array_key_exists($endDateEvent, $tasks)) {
