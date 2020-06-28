0fa8f5845dd1406d2edab10b2d37f842f3bc79fc
Nicolas <nmorero00@gmail.com>
2020-06-28 18:06:49 -0300

    reset imput file post

--- public/js/homeButtons.js
+++ public/js/homeButtons.js
@@ -24,6 +24,7 @@ $("#postBtnForm").on("submit", function(e) {
         console.log(res);
         document.getElementById("postBtnForm").reset();
         getPosts();
+        document.getElementById('PostBtnFile').value = "";
         document.getElementById('submitPost').disabled = false;
         $("#closePostBtn").click();
     });
